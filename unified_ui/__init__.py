898bdcef6cee7605a67584b73efb20539562d562
TSAI, CHEN-JIE <112858163+ch3njay@users.noreply.github.com>
2025-09-28 15:45:14 +0800

    Revert "Refactor theming to use config-driven design"

--- unified_ui/__init__.py
+++ unified_ui/__init__.py
@@ -1,6 +1,5 @@
 """D-FLARE Unified Interface Package."""
+
 from __future__ import annotations
 
-from . import theme_controller
-
-__all__ = ["theme_controller"]
+from . import theme_controller  # noqa: F401
